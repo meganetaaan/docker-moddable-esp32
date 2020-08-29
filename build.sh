7d0c2eecc22692f5131780eda9f4c2b0c6f25103
Tiryoh <tiryoh@gmail.com>
2020-08-29 23:10:37 +0900

    chore: Make the "latest" tag selective

--- build.sh
+++ build.sh
@@ -9,10 +9,20 @@ fi
 pushd moddable
     HASH=`git rev-parse --short HEAD`
 popd
-docker build -t tiryoh/moddable-esp32:latest .
-docker tag tiryoh/moddable-esp32:latest tiryoh/moddable-esp32:moddable-${HASH}
+docker build -t tiryoh/moddable-esp32:moddable-${HASH} .
+
+read -p "Is this image '*:latest'?(y/N): " yn
+case "$yn" in
+  [yY]*) IS_LATEST="True";;
+  *) IS_LATEST="";;
+esac
 
 read -p "Push to hub.docker.com?(y/N): " yn
 case "$yn" in [yY]*) ;; *) echo "Done" >&1 ; exit 0 ;; esac
-docker push tiryoh/moddable-esp32:latest
+
 docker push tiryoh/moddable-esp32:moddable-${HASH}
+
+if [ "${IS_LATEST}" -eq "True" ]; then
+	docker tag tiryoh/moddable-esp32:moddable-${HASH} tiryoh/moddable-esp32:latest
+	docker push tiryoh/moddable-esp32:latest
+fi
